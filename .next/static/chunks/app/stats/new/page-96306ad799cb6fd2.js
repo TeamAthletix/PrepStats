d512728dadfde511ed6f17ddc66b3f849d463555
Laory James <hoss@gcoastathletics.com>
2025-09-01 01:27:05 +0000

    chore: stop tracking .next build directory

--- .next/static/chunks/app/stats/new/page-96306ad799cb6fd2.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[125],{2001:function(e,r,t){Promise.resolve().then(t.bind(t,5874))},5874:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return StatEntryPage}});var n=t(7437);function StatEntryPage(){return(0,n.jsx)("div",{children:"Stat Entry Form (MVP placeholder)"})}},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),u=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,s={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,c={},i=null,_=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(_=r.ref),r)u.call(r,n)&&!s.hasOwnProperty(n)&&(c[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===c[n]&&(c[n]=r[n]);return{$$typeof:o,type:e,key:i,ref:_,props:c,_owner:f.current}}r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=2001)}),_N_E=e.O()}]);
