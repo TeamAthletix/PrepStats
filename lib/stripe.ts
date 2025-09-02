69d9b2a51314e503280fdd814e2bdbbca577b66b
Laory James <hoss@gcoastathletics.com>
2025-09-02 02:51:37 +0000

    Fix Stripe apiVersion mismatch

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@
 import { Stripe } from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-08-27.basil',
+  apiVersion: '2022-11-15',
 });
 
 export default stripe;
